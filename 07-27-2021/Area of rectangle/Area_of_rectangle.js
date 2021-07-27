c631af9dff1df9ef09b5f64666d972cba611175a
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-07-27 20:10:04 +0530

    07-27-2021

---
+++ 07-27-2021/Area of rectangle/Area_of_rectangle.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+let l=parseInt(readLine())
+let b=parseInt(readLine())
+if(l<0 || b<0){
+    console.log(0)
+}
+else{
+    console.log(l*b)
+}
---
+++ 07-27-2021/Len of Last word/Len_of_Last_word.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+  idx++;
+  return data[idx - 1];
+}
+
+// -------- Do NOT edit anything above this line ----------
+s=readLine()
+arr=s.split(" ")
+console.log(arr[arr.length-1].length)
